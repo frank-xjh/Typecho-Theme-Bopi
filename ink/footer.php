90a18d790c5f39e0d2aa2e05707de68b20c0ea15
frank-xjh <frank99-xu@outlook.com>
2018-11-13 11:23:50 +0800

    feat: jquery-pjax

--- ink/footer.php
+++ ink/footer.php
@@ -8,4 +8,21 @@
     <?php echo "\n"; ?>
     <?php echo $this->options->inFooter(); ?>
     <!--HTMLInFooter_end-->
+
+    <!--JS-->
+	<script src="//cdnjs.cloudflare.com/ajax/libs/mdui/0.4.2/js/mdui.min.js"></script>
+    <script src="//cdn.jsdelivr.net/npm/jquery@3.2/dist/jquery.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery.pjax/2.0.1/jquery.pjax.min.js"></script>
+    <script src="//unpkg.com/nprogress@0.2.0/nprogress.js"></script>
+
+    <!--Pjax-->
+    <script>
+	$(document).pjax('a[href^="<?php Helper::options()->siteUrl()?>"]:not(a[target="_blank"], a[no-pjax])', {
+   		container: '#pjax-container',
+    	fragment: '#pjax-container',
+    	timeout: 8000
+	})
+	$(document).on('pjax:start', function() { NProgress.start(); });
+	$(document).on('pjax:end',   function() { NProgress.done();  });
+	</script>
 </footer>
